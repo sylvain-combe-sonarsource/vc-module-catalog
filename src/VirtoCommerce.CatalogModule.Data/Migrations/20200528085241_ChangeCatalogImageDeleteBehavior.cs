8f877faad32a7c04dca5b6133327c54617b1804d
Aleksandr Andreev <alivemenone@gmail.com>
2020-05-28 12:02:34 +0300

    VP-2572 Fix problem with soft deleting CatalogImage entities Change CatalogImage delete behavior Add migration

---
+++ src/VirtoCommerce.CatalogModule.Data/Migrations/20200528085241_ChangeCatalogImageDeleteBehavior.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.CatalogModule.Data.Migrations
+{
+    public partial class ChangeCatalogImageDeleteBehavior : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CatalogImage_Category_CategoryId",
+                table: "CatalogImage");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CatalogImage_Item_ItemId",
+                table: "CatalogImage");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CatalogImage_Category_CategoryId",
+                table: "CatalogImage",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CatalogImage_Item_ItemId",
+                table: "CatalogImage",
+                column: "ItemId",
+                principalTable: "Item",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CatalogImage_Category_CategoryId",
+                table: "CatalogImage");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CatalogImage_Item_ItemId",
+                table: "CatalogImage");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CatalogImage_Category_CategoryId",
+                table: "CatalogImage",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CatalogImage_Item_ItemId",
+                table: "CatalogImage",
+                column: "ItemId",
+                principalTable: "Item",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
